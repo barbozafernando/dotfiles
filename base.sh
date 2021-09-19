b39fd22ed847f62c5fbf2a0c86c06e32470b453d
Fernando Barboza <falandocomofernando@gmail.com>
2021-09-19 20:34:21 -0300

    Added brave installation

--- base.sh
+++ base.sh
@@ -11,22 +11,36 @@ sudo apt update && sudo apt upgrade
 echo "Installing curl..."
 sudo apt install curl
 
-echo "Downloading my init.vim file..."
 if [ -d ~/.config/nvim ]; then
   if [ ! -e ~/.config/nvim/init.vim ]; then
+    echo "Downloading my init.vim file..."
     curl -o ~/.config/nvim/init.vim https://raw.githubusercontent.com/barbozafernando/dotfiles/master/init.vim
   fi
 else
   mkdir ~/.config/nvim
+  echo "Downloading my init.vim file..."
   curl -o ~/.config/nvim/init.vim https://raw.githubusercontent.com/barbozafernando/dotfiles/master/init.vim
 fi
 
-echo "Downloading vim-plug for vim..."
 if [ -d ~/.config/nvim/autoload ]; then
   if [ ! -e ~/.config/nvim/autoload/plug.vim ]; then
+    echo "Downloading vim-plug for vim..."
     curl -o ~/.config/nvim/autoload/plug.vim https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
   fi
 else
   mkdir ~/.config/nvim/autoload
+  echo "Downloading vim-plug for vim..."
   curl -o ~/.config/nvim/autoload/plug.vim https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 fi
+
+# Didnt test it yet
+echo "Installing brave browser..."
+sudo apt install apt-transport-https 
+
+sudo curl -fsSLo /usr/share/keyrings/brave-browser-archive-keyring.gpg https://brave-browser-apt-release.s3.brave.com/brave-browser-archive-keyring.gpg
+
+echo "deb [signed-by=/usr/share/keyrings/brave-browser-archive-keyring.gpg arch=amd64] https://brave-browser-apt-release.s3.brave.com/ stable main"|sudo tee /etc/apt/sources.list.d/brave-browser-release.list
+
+sudo apt update
+
+sudo apt install brave-browser
