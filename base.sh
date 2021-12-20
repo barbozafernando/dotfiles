14e1f68e3bc3a007332227024e6f73ef25e70c58
Fernando Barboza <falandocomofernando@gmail.com>
2021-12-20 20:43:43 -0300

    Update base.sh

--- base.sh
+++ base.sh
@@ -4,13 +4,16 @@
 # python3 install.py --js-completer --ts-completer
 # to install semantic completion to js/ts
 
+LOG_FILE=base.log
+CURRENT_LINE=$LINENO
+
 echo "Installing git..."
 sudo apt-get update && sudo apt-get install git-all
 
 echo "Installing python, cmake for the YouCompleteMe vim plugin..."
 sudo apt install build-essential cmake python3-dev
 
-echo "Installing vim..."
+echo "Installing nvim..."
 sudo apt-get update && sudo apt-get install neovim
 
 sudo apt update && sudo apt upgrade
@@ -46,10 +49,26 @@ else
   curl -o ~/.config/nvim/autoload/plug.vim https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 fi
 
-# Didnt test it yet
 echo "Installing brave browser..."
 sudo apt install apt-transport-https 
 sudo curl -fsSLo /usr/share/keyrings/brave-browser-archive-keyring.gpg https://brave-browser-apt-release.s3.brave.com/brave-browser-archive-keyring.gpg
 echo "deb [signed-by=/usr/share/keyrings/brave-browser-archive-keyring.gpg arch=amd64] https://brave-browser-apt-release.s3.brave.com/ stable main"|sudo tee /etc/apt/sources.list.d/brave-browser-release.list
 sudo apt update
 sudo apt install brave-browser
+
+echo "Installing fish shell..."
+sudo apt-add-repository ppa:fish-shell/release-3
+sudo apt-get update
+sudo apt-get install fish
+
+# Fish plugin manager
+echo "Installing fisher..."
+sudo apt-get update
+curl -sL https://git.io/fisher | source && fisher install jorgebucaran/fisher
+
+if fisher -v; then
+  echo "Installing tide theme for fish shell..."
+  fisher install IlanCosman/tide@v5
+else
+  echo "ERROR: Couldn't install tide, line: $CURRENT_LINE" >> $LOG_FILE
+fi
