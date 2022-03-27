2e9e5640e7ccc2313a53499b918b47d632981829
Fernando Barboza <falandocomofernando@gmail.com>
2022-03-27 12:02:30 -0300

    Update base.sh

--- base.sh
+++ base.sh
@@ -13,62 +13,45 @@ sudo apt-get update && sudo apt-get install git-all
 echo "Installing python, cmake for the YouCompleteMe vim plugin..."
 sudo apt install build-essential cmake python3-dev
 
-echo "Installing nvim..."
+echo "Installing lastest version of nvim..."
+sudo add-apt-repository ppa:neovim-ppa/stable
 sudo apt-get update && sudo apt-get install neovim
 
-sudo apt update && sudo apt upgrade
-
 echo "Installing curl..."
+sudo apt update &&
 sudo apt install curl
 
 echo "Installing node..."
+sudo apt update &&
 curl -fsSL https://deb.nodesource.com/setup_16.x | sudo -E bash -
 sudo apt-get install -y nodejs
 
+echo "Downloading my vim settings files..."
 if [ -d ~/.config/nvim ]; then
   if [ ! -e ~/.config/nvim/init.vim ]; then
-    echo "Downloading my vim settings files..."
     cd ~/.config/nvim
     curl https://codeload.github.com/barbozafernando/dotfiles/tar.gz/master | \tar -xz --strip=2 dotfiles-master/nvim
   fi
 else
-  echo "Downloading my vim settings files..."
   mkdir ~/.config/nvim
-  cd ~/.config/nvim
+  cd ~/.config/nvim &&
   curl https://codeload.github.com/barbozafernando/dotfiles/tar.gz/master | \tar -xz --strip=2 dotfiles-master/nvim
 fi
 
+echo "Downloading vim-plug for vim..."
 if [ -d ~/.config/nvim/autoload ]; then
   if [ ! -e ~/.config/nvim/autoload/plug.vim ]; then
-    echo "Downloading vim-plug for vim..."
     curl -o ~/.config/nvim/autoload/plug.vim https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
   fi
 else
-  echo "Downloading vim-plug for vim..."
-  mkdir ~/.config/nvim/autoload
+  mkdir ~/.config/nvim/autoload &&
   curl -o ~/.config/nvim/autoload/plug.vim https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 fi
 
 echo "Installing brave browser..."
-sudo apt install apt-transport-https 
+sudo apt install apt-transport-https &&
 sudo curl -fsSLo /usr/share/keyrings/brave-browser-archive-keyring.gpg https://brave-browser-apt-release.s3.brave.com/brave-browser-archive-keyring.gpg
 echo "deb [signed-by=/usr/share/keyrings/brave-browser-archive-keyring.gpg arch=amd64] https://brave-browser-apt-release.s3.brave.com/ stable main"|sudo tee /etc/apt/sources.list.d/brave-browser-release.list
-sudo apt update
+sudo apt update &&
 sudo apt install brave-browser
 
-echo "Installing fish shell..."
-sudo apt-add-repository ppa:fish-shell/release-3
-sudo apt-get update
-sudo apt-get install fish
-
-# Fish plugin manager
-echo "Installing fisher..."
-sudo apt-get update
-curl -sL https://git.io/fisher | source && fisher install jorgebucaran/fisher
-
-if fisher -v; then
-  echo "Installing tide theme for fish shell..."
-  fisher install IlanCosman/tide@v5
-else
-  echo "ERROR: Couldn't install tide, line: $CURRENT_LINE" >> $LOG_FILE
-fi
