53f5cfaf08c6d5288dc1f383990af0d14054a8e5
Fernando Barboza <falandocomofernando@gmail.com>
2022-04-25 21:51:08 -0300

    changes

--- base.sh
+++ base.sh
@@ -4,69 +4,90 @@
 # python3 install.py --js-completer --ts-completer
 # to install semantic completion to js/ts
 
-LOG_FILE=base.log
-CURRENT_LINE=$LINENO
+LOG_FILE=base.txt
 
-echo "Installing git..."
-sudo apt-get update && sudo apt-get install git-all
+sudo apt-get update && sudo apt-get install git-all -y
+if [ $? -eq 0 ]; then
+   echo "Git installed..." >> $LOG_FILE
+else
+   echo "Fail to install git." >> $LOG_FILE
+fi
 
-echo "Installing python, cmake for the YouCompleteMe vim plugin..."
-sudo apt install build-essential cmake python3-dev
+sudo apt install build-essential cmake python3-dev -y
+if [ $? -eq 0 ]; then
+   echo "Python and cmake installed..." >> $LOG_FILE
+else
+   echo "Fail to install python and cmake.." >> $LOG_FILE
+fi
 
-echo "Installing lastest version of nvim..."
 sudo add-apt-repository ppa:neovim-ppa/stable
 sudo apt-get update && sudo apt-get install neovim
+if [ $? -eq 0 ]; then
+   echo "NVIM installed..." >> $LOG_FILE
+else
+   echo "Fail to install nvim..." >> $LOG_FILE
+fi
 
-echo "Installing curl..."
-sudo apt update &&
-sudo apt install curl
+sudo apt update && sudo apt install curl -y
+if [ $? -eq 0 ]; then
+   echo "curl installed..." >> $LOG_FILE
+else
+   echo "Fail to install curl..." >> $LOG_FILE
+fi
 
-echo "Installing node..."
-sudo apt update &&
-curl -fsSL https://deb.nodesource.com/setup_16.x | sudo -E bash -
+sudo apt update && curl -fsSL https://deb.nodesource.com/setup_16.x | sudo -E bash -
 sudo apt-get install -y nodejs
+if [ $? -eq 0 ]; then
+   echo "Node installed..." >> $LOG_FILE
+else
+   echo "Fail to install node..." >> $LOG_FILE
+fi
+
+sudo apt install apt-transport-https &&
+sudo curl -fsSLo /usr/share/keyrings/brave-browser-archive-keyring.gpg https://brave-browser-apt-release.s3.brave.com/brave-browser-archive-keyring.gpg
+echo "deb [signed-by=/usr/share/keyrings/brave-browser-archive-keyring.gpg arch=amd64] https://brave-browser-apt-release.s3.brave.com/ stable main"|sudo tee /etc/apt/sources.list.d/brave-browser-release.list
+sudo apt update && sudo apt install brave-browser
+if [ $? -eq 0 ]; then
+   echo "Brave installed..." >> $LOG_FILE
+else
+   echo "Fail to install brave..." >> $LOG_FILE
+fi
 
 import_vim_settings
 import_vim_plug
 
-echo "Installing brave browser..."
-sudo apt install apt-transport-https &&
-sudo curl -fsSLo /usr/share/keyrings/brave-browser-archive-keyring.gpg https://brave-browser-apt-release.s3.brave.com/brave-browser-archive-keyring.gpg
-echo "deb [signed-by=/usr/share/keyrings/brave-browser-archive-keyring.gpg arch=amd64] https://brave-browser-apt-release.s3.brave.com/ stable main"|sudo tee /etc/apt/sources.list.d/brave-browser-release.list
-sudo apt update &&
-sudo apt install brave-browser
-
 function import_vim_settings {
-	echo "Importing vim settings..."
 	if [ ! -d ~/.config ]; then
-		mkdir ~/.config && mkdir ~/.config/nvim && cd ~/.config/nvim
-		curl https://codeload.github.com/barbozafernando/dotfiles/tar.gz/master | \tar -xz --strip=2 dotfiles-master/nvim
-		echo "Vim settings imported successfully."
-		return;
+		mkdir ~/.config
 	fi
 
 	if [ ! -d ~/.config/nvim ]; then
-		mkdir ~/.config/nvim && cd ~/.config/nvim
-		curl https://codeload.github.com/barbozafernando/dotfiles/tar.gz/master | \tar -xz --strip=2 dotfiles-master/nvim
-		echo "Vim settings imported successfully."
-		return;
+		mkdir ~/.config/nvim
 	fi
 
 	cd ~/.config/nvim && curl https://codeload.github.com/barbozafernando/dotfiles/tar.gz/master | \tar -xz --strip=2 dotfiles-master/nvim
-	echo "Vim settings imported successfully."
+
+  if [ $? -eq 0 ]; then
+    echo "Vim settings imported successfully." >> $LOG_FILE
+  else
+     echo "Fail to import vim settings." >> $LOG_FILE
+  fi
 }
 
 function import_vim_plug {
-	echo "Importing vim plug..."
 	if [ ! -d ~/.config/nvim/autoload ]; then
 		mkdir ~/.config/nvim/autoload
 	fi
 
-	if [ ! -e ~/.config/nvim/autoload/plug.vim ]; then
-		curl -o ~/.config/nvim/autoload/plug.vim https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
-		echo "Vim plug imported successfully."
+	if [ -e ~/.config/nvim/autoload/plug.vim ]; then
+    return
 	fi
 
-	echo "Vim plug is already imported."
+	curl -o ~/.config/nvim/autoload/plug.vim https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+  if [ $? -eq 0 ]; then
+    echo "Vim plug was imported successfully." >> $LOG_FILE
+  else
+    echo "Fail to import vim plug." >> $LOG_FILE
+  fi
 }
 
