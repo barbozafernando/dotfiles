eed87f262ce3f8ab92fd520007ed8f8f3b21e312
Fernando Barboza <falandocomofernando@gmail.com>
2024-07-10 20:47:43 -0300

    Update telescope.lua

--- nvim/after/plugin/telescope.lua
+++ nvim/after/plugin/telescope.lua
@@ -2,8 +2,8 @@ local builtin = require('telescope.builtin')
 
 vim.keymap.set('n', '<C-p>', builtin.git_files, {})
 vim.keymap.set('n', '<C-h>', builtin.live_grep, {})
-vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
-vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
+vim.keymap.set('n', '<leader>pb', builtin.buffers, {})
+vim.keymap.set('n', '<leader>ph', builtin.help_tags, {})
 vim.keymap.set('n', '<leader>pf', builtin.find_files, {})
 vim.keymap.set('n', '<leader>ps', function()
 	builtin.grep_string({ search = vim.fn.input("Grep > ") });
