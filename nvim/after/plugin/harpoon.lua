247c65c5fc54b949f34d1a5e42e057a2bbf531d4
Fernando Barboza <falandocomofernando@gmail.com>
2024-07-10 20:23:56 -0300

    Create harpoon.lua

---
+++ nvim/after/plugin/harpoon.lua
@@ -0,0 +1,7 @@
+local mark = require("harpoon.mark")
+local ui = require("harpoon.ui")
+
+vim.keymap.set("n", "<C-a>", mark.add_file)
+vim.keymap.set("n", "<C-e>", ui.toggle_quick_menu)
+vim.keymap.set("n", "<C-n>", ui.nav_next)
+vim.keymap.set("n", "<C-t>", ui.nav_prev)
