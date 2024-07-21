70eb3f490babd0a5e95202a57f3a13c3d1ba7f19
barbozafernando <falandocomofernando@gmail.com>
2024-07-21 13:47:30 -0300

    Create easyalign.lua

---
+++ nvim/after/plugin/easyalign.lua
@@ -0,0 +1,10 @@
+vim.api.nvim_set_keymap('x', 'ga', '<Plug>(EasyAlign)', { noremap = false, silent = true })
+vim.api.nvim_set_keymap('n', 'ga', '<Plug>(EasyAlign)', { noremap = false, silent = true })
+
+--vipga=
+    --visual-select inner paragraph
+    --Start --EasyAlign command (ga)
+    --Align around --=
+--gaip=
+    --Start EasyAlign command (ga) for inner paragraph
+    --Align --around =
